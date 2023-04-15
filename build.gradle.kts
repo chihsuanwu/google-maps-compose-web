57d963d93c1d26ee7e83bfbf37d681b72513a559
Chi-Hsuan Wu <chihsuanw@gmail.com>
2023-04-15 11:37:34 +0800

    Fixed node version due to Jitpack's issue. (similar error message to https://discuss.kotlinlang.org/t/how-can-i-change-node-js-version-in-kotlin-js-project/26648)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,11 @@ plugins {
     kotlin("multiplatform") apply false
     id("org.jetbrains.compose") apply false
 }
+
+// The node version that kotlin 1.8.20 uses is too new for Jitpack.
+// So we fix the node version to 16.20.0.
+plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+        nodeVersion = "16.20.0"
+    }
+}
